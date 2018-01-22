de237376d03f630a1b9f722c7ae0094b06aeeb09
Benny <35159859+DetectiveSax@users.noreply.github.com>
2018-01-21 19:05:48 -0800

    not done yet

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string name01;
+
+            Console.Write("Hello user. My name is Al. What is your name?");
+
+           name01 = (Console.ReadLine());
+
+            Console.Write("Hello");
+            Console.ReadLine();
+            
+
+            
+
+        }
+    }
+}
